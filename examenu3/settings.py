f07c72d427a9f7c964130cbc0044a665eca87042
JorgeRG <al221211277@gmail.com>
2021-04-06 09:43:11 -0500

    segunda version en git

--- examenu3/settings.py
+++ examenu3/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'examen',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'examenu3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['examen/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
